d4477ce8c03124d645a36c60cefe9c0128530ec8
chongyang zhang <51123654+zcy1010@users.noreply.github.com>
2022-07-12 18:30:15 +0800

    user manager

--- apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/repository/UserRepository.java
+++ apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/repository/UserRepository.java
@@ -29,8 +29,6 @@ public interface UserRepository extends PagingAndSortingRepository<UserPO, Long>
 
   List<UserPO> findFirst20ByEnabled(int enabled);
 
-  List<UserPO> findAllByEnabled(int enabled);
-
   List<UserPO> findByUsernameLikeAndEnabled(String username, int enabled);
 
   List<UserPO> findByUserDisplayNameLikeAndEnabled(String userDisplayName, int enabled);
